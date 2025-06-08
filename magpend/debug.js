4deb5b61e5866c40cb4c546117da36ee1e469922
hclind <hclind@gmail.com>
2025-06-09 00:20:14 +0800

    update

---
+++ magpend/debug.js
@@ -0,0 +1,14 @@
+var Debug = /** @class */ (function () {
+    function Debug() {
+    }
+    Debug.assert = function (condition, message) {
+        if (message === void 0) { message = "Assertion failed"; }
+        if (!condition) {
+            if (typeof Error !== "undefined") {
+                throw new Error(message);
+            }
+            throw message;
+        }
+    };
+    return Debug;
+}());
